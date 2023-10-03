2384a52306207d97cfb3d7d9ade0b674b9ba33f8
Ashi <144452098+ashi1072004@users.noreply.github.com>
2023-10-04 04:44:07 +0500

    testing done

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
-// var nav = document.querySelector('nav');
-    // window.addEventListener('scroll', function () {
-    //     if (window.pageYOffset > 100) {
-    //         nav.classList.add('bg-secondary', 'shadow');
-    //     }
-    //     else {
-    //         nav.classList.remove('bg-secondary', 'shadow');
-    //     }
-    // });
+describe("pow", function(){
+    describe("raises x to power 3", function(){
+        function makeTest(x){
+            let expected = x**3;
+            it(`${x} to power 3 is ${expected}`, function(){
+                assert.equal(pow(x,3), expected);
+            })
+        }
+        for(x=1; x<=5; x++){
+            makeTest(x);
+        }
+    });
+    describe("raises x to power 4", function(){
+        function makeTest(x){
+            let expected = x**4;
+            it(`${x} to power 4 is ${expected}`, function(){
+                assert.equal(pow(x,4), expected);
+            })
+        }
+        for(x=1; x<=5; x++){
+            makeTest(x);
+        }
+    });
+});
